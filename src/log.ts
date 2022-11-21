4280976bc91c11496b6a608e6e723067d6dc1e64
C. Lewis <ctj.lewis@icloud.com>
2022-11-21 09:29:14 -0600

    chore: check TTY at API level

--- src/log.ts
+++ src/log.ts
@@ -2,38 +2,24 @@
 import { style } from "./style";
 import { LogStyles, PaddingOptions } from "./types";
 
-const getStyledMessage = (
-  message: string,
-  styles?: LogStyles[],
-  { indent, newlines }: PaddingOptions = { indent: 2, newlines: 1 }
-) => {
-  const { isTTY } = process.stdout;
-
-  const styled = isTTY ? style(message, styles) : message;
-  const lineStartSpacing = " ".repeat(indent);
-  const lineEndSpacing = "\n".repeat(newlines);
-
-  return (
-    lineStartSpacing +
-    styled +
-    lineEndSpacing
-  );
-};
-
+/**
+ * Styled `console.log`.
+ */
 export const log = (
   message: string,
   styles?: LogStyles[],
   padding?: PaddingOptions
 ) => {
-  const styledMessage = getStyledMessage(message, styles, padding);
-  console.log(styledMessage);
+  console.log(style(message, styles, padding));
 };
 
+/**
+ * Styled `console.error`.
+ */
 export const error = (
   message: string,
   styles?: LogStyles[],
   padding?: PaddingOptions
 ) => {
-  const styledMessage = getStyledMessage(message, styles, padding);
-  console.error(styledMessage);
+  console.error(style(message, styles, padding));
 };
--- src/style.ts
+++ src/style.ts
@@ -1,19 +1,43 @@
 import ansiStyles from "ansi-styles";
-import { LogStyles } from "./types";
+import { LogStyles, PaddingOptions } from "./types";
 
-export const style = (message: string, styles?: LogStyles[]) => {
-  if (!styles) {
-    return message;
+/**
+ * Apply ANSI styles to a message, as well as a given indent and newline
+ * padding.
+ *
+ * If `process.stdout` is not a TTY, the message will not have ANSI styles
+ * applied.
+ */
+export const style = (
+  message: string,
+  styles?: LogStyles[],
+  { indent, newlines }: PaddingOptions = { indent: 2, newlines: 1 }
+) => {
+  /**
+   * If styles are provided and stdout is TTY, then apply styles.
+   */
+  if (styles && process.stdout.isTTY) {
+    let opening = "";
+    let closing = "";
+
+    for (const style of styles) {
+      const ansiStyle = ansiStyles[style];
+      opening += ansiStyle.open;
+      closing += ansiStyle.close;
+    }
+
+    message = opening + message + closing;
   }
 
-  let opening = "";
-  let closing = "";
+  const lineStartSpacing = " ".repeat(indent);
+  const lineEndSpacing = "\n".repeat(newlines);
 
-  for (const style of styles) {
-    const ansiStyle = ansiStyles[style];
-    opening += ansiStyle.open;
-    closing += ansiStyle.close;
-  }
-
-  return opening + message + closing;
+  /**
+   * Apply indent and spacing.
+   */
+  return (
+    lineStartSpacing +
+    message +
+    lineEndSpacing
+  );
 };
