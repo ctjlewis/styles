ab94cd2a25e02b4142ec92cc52faf01e2ccb3bf6
C. Lewis <ctj.lewis@icloud.com>
2022-11-22 16:20:38 -0600

    v1.1.1

--- src/padProcess.ts
+++ src/padProcess.ts
@@ -1,12 +1,26 @@
+/**
+ * @fileoverview
+ *
+ * Ensure there is minium vertical and horizontal padding for this process's
+ * logs.
+ *
+ * Involves an `exit` listener and process.env variables. Fun!
+ */
 import { env } from "process";
 import { group, log } from "./log";
 
-const addedInitialPadding = env["__ADDED_INITIAL_PADDING__"] || null;
+const ADDED_INITIAL_PADDING = "__ADDED_INITIAL_PADDING__";
+const ADDED_FINAL_PADDING = "__ADDED_FINAL_PADDING__";
+
+const addedInitialPadding = env[ADDED_INITIAL_PADDING] || null;
+const addedFinalPadding = env[ADDED_FINAL_PADDING] || null;
 
 const addFinalPadding = () => {
   log();
   log();
   group.end();
+
+  env[ADDED_FINAL_PADDING] = "true";
 };
 
 const addInitialPadding = () => {
@@ -14,16 +28,21 @@ const addInitialPadding = () => {
   log();
   group.start();
 
-  env["__ADDED_INITIAL_PADDING__"] = "true";
+  env[ADDED_INITIAL_PADDING] = "true";
 };
 
+/**
+ * Force initial padding for this process.
+ */
 if (!addedInitialPadding) {
   addInitialPadding();
 }
 
 /**
- * Force padding on process close. Fun!
+ * Force padding on process close.
  */
-process.addListener("exit", addFinalPadding);
+if (!addedFinalPadding) {
+  process.on("exit", addFinalPadding);
+}
 
 export {};
