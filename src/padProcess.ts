1c0d9c11b82c24e71e9c1a6a5ca54b6a379098ae
C. Lewis <ctj.lewis@icloud.com>
2022-11-22 16:56:52 -0600

    fix: `padProcess.ts`

--- src/padProcess.ts
+++ src/padProcess.ts
@@ -4,23 +4,24 @@
  * Ensure there is minium vertical and horizontal padding for this process's
  * logs.
  *
- * Involves an `exit` listener and process.env variables. Fun!
+ * Involves an `exit` listener and process.env variables, and making sure both
+ * padding logs only run once per process. Fun!
  */
 import { env } from "process";
 import { group, log } from "./log";
 
 const ADDED_INITIAL_PADDING = "__ADDED_INITIAL_PADDING__";
-const ADDED_FINAL_PADDING = "__ADDED_FINAL_PADDING__";
+const SET_FiNAL_PADDING_LISTENER = "__SET_FiNAL_PADDING_LISTENER__";
 
-const addedInitialPadding = env[ADDED_INITIAL_PADDING] || null;
-const addedFinalPadding = env[ADDED_FINAL_PADDING] || null;
+const setInitialPadding = () => env[ADDED_INITIAL_PADDING] || null;
+const setFinalPaddingListener = () => env[SET_FiNAL_PADDING_LISTENER] || null;
 
 const addFinalPadding = () => {
   log();
   log();
   group.end();
 
-  env[ADDED_FINAL_PADDING] = "true";
+  process.removeListener("exit", addFinalPadding);
 };
 
 const addInitialPadding = () => {
@@ -34,15 +35,16 @@ const addInitialPadding = () => {
 /**
  * Force initial padding for this process.
  */
-if (!addedInitialPadding) {
+if (!setInitialPadding()) {
   addInitialPadding();
 }
 
 /**
  * Force padding on process close.
  */
-if (!addedFinalPadding) {
+if (!setFinalPaddingListener()) {
   process.on("exit", addFinalPadding);
+  env[SET_FiNAL_PADDING_LISTENER] = "true";
 }
 
 export {};
