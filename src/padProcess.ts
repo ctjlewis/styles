efcc854b2e9fb086d6bb81f6540474b979b22559
C. Lewis <ctj.lewis@icloud.com>
2022-12-03 17:12:19 -0600

    fix: do not pad non-TTY process

--- src/padProcess.ts
+++ src/padProcess.ts
@@ -7,51 +7,53 @@
  * Involves an `exit` listener and process.env variables, and making sure both
  * padding logs only run once per process. Fun!
  */
-import { env } from "process";
+import { env, stdout } from "process";
 import { group, log } from "./log";
 
-const ADDED_INITIAL_PADDING = "__ADDED_INITIAL_PADDING__";
-const SET_FINAL_PADDING_LISTENER = "__SET_FiNAL_PADDING_LISTENER__";
+if (stdout.isTTY) {
+  const ADDED_INITIAL_PADDING = "__ADDED_INITIAL_PADDING__";
+  const SET_FINAL_PADDING_LISTENER = "__SET_FiNAL_PADDING_LISTENER__";
 
-const setInitialPadding = () => env[ADDED_INITIAL_PADDING] || null;
-const setFinalPaddingListener = () => env[SET_FINAL_PADDING_LISTENER] || null;
+  const setInitialPadding = () => env[ADDED_INITIAL_PADDING] || null;
+  const setFinalPaddingListener = () => env[SET_FINAL_PADDING_LISTENER] || null;
 
-const addInitialPadding = () => {
-  log();
-  // log();
-  group.start();
+  const addInitialPadding = () => {
+    log();
+    // log();
+    group.start();
 
-  env[ADDED_INITIAL_PADDING] = "true";
-};
+    env[ADDED_INITIAL_PADDING] = "true";
+  };
 
-const addFinalPadding = () => {
-  log();
-  // log();
-  group.end();
+  const addFinalPadding = () => {
+    log();
+    // log();
+    group.end();
 
-  process.removeListener("exit", addFinalPadding);
-};
+    process.removeListener("exit", addFinalPadding);
+  };
 
-/**
+  /**
  * Force initial padding for this process.
  */
-if (!setInitialPadding()) {
-  addInitialPadding();
-}
+  if (!setInitialPadding()) {
+    addInitialPadding();
+  }
 
-/**
+  /**
  * Force padding on process close.
  */
-if (!setFinalPaddingListener()) {
-  process.on("exit", addFinalPadding);
-  env[SET_FINAL_PADDING_LISTENER] = "true";
-}
+  if (!setFinalPaddingListener()) {
+    process.on("exit", addFinalPadding);
+    env[SET_FINAL_PADDING_LISTENER] = "true";
+  }
 
-/**
+  /**
  * Ensures the process.exit() event fires, including on Windows.
  */
-process.addListener("SIGINT", () => {
-  process.exit(1);
-});
+  process.addListener("SIGINT", () => {
+    process.exit(1);
+  });
+}
 
 export {};
